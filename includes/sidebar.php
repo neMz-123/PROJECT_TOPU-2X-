79bf7139f37280493b1b82380d9b135d3927f776
neMz-123 <143060243+neMz-123@users.noreply.github.com>
2024-05-18 20:54:09 +0800

    trial

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -13,6 +13,18 @@
       </li><!-- End Dashboard Nav -->
 
       <li class="nav-item">
+        <a class="nav-link " href="products.php">
+          <i class="bi bi-grid"></i>
+          <span>Products</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+
+
+
+
+
+      <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
---
+++ products.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include 'includes/head.php';
+include 'includes/header.php';
+?>
+
+<body>
+
+  <h1>PRODUCTS</h1>
+
+
+    
+</body>
+<?php
+include 'includes/footer.php';
+?>
+</html>
