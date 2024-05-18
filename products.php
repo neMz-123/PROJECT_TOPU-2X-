8269f05694085e8b03e9c4d1115072cc8816c071
neMz-123 <143060243+neMz-123@users.noreply.github.com>
2024-05-18 23:06:31 +0800

    edit

--- products.php
+++ products.php
@@ -6,6 +6,7 @@
 include 'includes/head.php';
 ?>
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
 <body>
 
@@ -25,7 +26,19 @@ include 'includes/sidebar.php';
           <li class="breadcrumb-item active">Dashboard</li>
         </ol>
       </nav>
-    </div><!-- End Page Title -->
+    </div>
+    <!-- End Page Title -->
+   
+    
+
+    <div class="button-row" style="display: flex; justify-content: flex-end;">
+        <button type="button" class="btn btn-primary">
+            <i class="fas fa-plus"></i> Add Products
+        </button> 
+    </div>
+
+
+    <br>
 
     <section class="section dashboard">
 
@@ -46,17 +59,22 @@ include 'includes/sidebar.php';
                   </ul>
                 </div>
 
+             
+
+
                 <div class="card-body pb-0">
-                  <h5 class="card-title">Top Selling <span>| Today</span></h5>
+                  <h5 class="card-title">PRODUCTS<span>| List of Products</span></h5>
 
                   <table class="table table-borderless">
                     <thead>
                       <tr>
                         <th scope="col">Preview</th>
                         <th scope="col">Product</th>
+                        <th scope="col">Product ID</th>
+                        <th scope="col">Product Description</th>
                         <th scope="col">Price</th>
-                        <th scope="col">Sold</th>
-                        <th scope="col">Revenue</th>
+                        <th scope="col">Actions</th>
+
                       </tr>
                     </thead>
                     <tbody>
@@ -64,8 +82,13 @@ include 'includes/sidebar.php';
                         <th scope="row"><a href="#"><img src="assets/img/milo.jpg" alt=""></a></th>
                         <td><a href="#" class="text-primary fw-bold">MILO</a></td>
                         <td> ₱ 150.00</td>
-                        <td class="fw-bold">124</td>
+                        <td class="fw-bold">qwwereytruytiuuyiouoiuoi</td>
                         <td>₱ 30.00</td>
+                        <td>
+                            <img src="assets/img/edit.png" alt="Edit" title="Edit" style="height:25px; width:25px;">
+                            <img src="assets/img/delete.png" alt="Delete" title="Delete"style="height:25px; width:25px;">
+                        </td>
+
                       </tr>
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/bearbrand.jpg" alt=""></a></th>
@@ -73,6 +96,12 @@ include 'includes/sidebar.php';
                         <td> ₱ 20.00</td>
                         <td class="fw-bold">98</td>
                         <td>₱ 5.00</td>
+                        <td>
+                            <img src="assets/img/edit.png" alt="Edit" title="Edit" style="height:25px; width:25px;">
+                            <img src="assets/img/delete.png" alt="Delete" title="Delete"style="height:25px; width:25px;">
+                        </td>
+                     
+
                       </tr>
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/yakult.png" alt=""></a></th>
@@ -80,6 +109,10 @@ include 'includes/sidebar.php';
                         <td>₱ 16.00</td>
                         <td class="fw-bold">74</td>
                         <td>₱ 5.00</td>
+                        <td>
+                            <img src="assets/img/edit.png" alt="Edit" title="Edit" style="height:25px; width:25px;">
+                            <img src="assets/img/delete.png" alt="Delete" title="Delete"style="height:25px; width:25px;">
+                        </td>
                       </tr>
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/nestea.png" alt=""></a></th>
@@ -87,18 +120,27 @@ include 'includes/sidebar.php';
                         <td>₱ 45.00</td>
                         <td class="fw-bold">63</td>
                         <td>₱ 5.00</td>
+                        <td>
+                            <img src="assets/img/edit.png" alt="Edit" title="Edit" style="height:25px; width:25px;">
+                            <img src="assets/img/delete.png" alt="Delete" title="Delete"style="height:25px; width:25px;">
+                        </td>
                       </tr>
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/waffel.png" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">WAFFEL HOUSE</a></td>
+                        <td><a href="#" class="text-primary fw-bold">WAFFEL HAUS</a></td>
                         <td>₱ 55.00</td>
                         <td class="fw-bold">41</td>
                         <td>₱ 5.00</td>
+                        <td>
+                            <img src="assets/img/edit.png" alt="Edit" title="Edit" style="height:25px; width:25px;">
+                            <img src="assets/img/delete.png" alt="Delete" title="Delete"style="height:25px; width:25px;">
+                        </td>
                       </tr>
                     </tbody>
                   </table>
 
                 </div>
+               
 
               </div>
             </div>
