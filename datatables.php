753580b2903fbc6abe952608aa77b5df205ab030
neMz-123 <143060243+neMz-123@users.noreply.github.com>
2024-05-18 21:24:33 +0800

    edit

--- datatables.php
+++ datatables.php
@@ -1,14 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
 <?php
 include 'includes/head.php';
-include 'includes/sidebar.php';
-include 'includes/header.php';
-
-
 ?>
+
+
 <body>
- 
- <!-- Recent Sales -->
- <div class="col-12">
+
+<?php
+include 'includes/header.php';
+include 'includes/sidebar.php';
+?>
+
+
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>POS - DATA TABLES</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+          <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+      <div class="row">
+
+        <!-- Left side columns -->
+        <div class="col-lg-8">
+          <div class="row">
+
+
+            <!-- Recent Sales -->
+            <div class="col-12">
               <div class="card recent-sales overflow-auto">
 
                 <div class="filter">
@@ -81,6 +110,48 @@ include 'includes/header.php';
               </div>
             </div><!-- End Recent Sales -->
 
-            <?php
-include 'includes/footer.php';
-?>
+            
+
+           
+          
+
+           
+
+          </div>
+        </div><!-- End Left side columns -->
+
+        <!-- Right side columns -->
+        <div class="col-lg-4">
+
+         
+          
+
+          
+
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <?php
+  include 'includes/footer.php';
+  ?>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
--- products.php
+++ products.php
@@ -1,17 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
 <?php
 include 'includes/head.php';
-include 'includes/sidebar.php';
-include 'includes/header.php';
-
-
 ?>
+
+
 <body>
 
- <!-- Top Selling -->
- <div class="col-12">
+<?php
+include 'includes/header.php';
+include 'includes/sidebar.php';
+?>
+
+
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>POS - PRODUCTS</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+          <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+      <div class="row">
+
+        <!-- Left side columns -->
+        <div class="col-lg-8">
+          <div class="row">
+
+
+           <!-- Top Selling -->
+           <div class="col-12">
               <div class="card top-selling overflow-auto">
 
                 <div class="filter">
@@ -84,9 +110,48 @@ include 'includes/header.php';
               </div>
             </div><!-- End Top Selling -->
 
-    
+            
+
+           
+          
+
+           
+
+          </div>
+        </div><!-- End Left side columns -->
+
+        <!-- Right side columns -->
+        <div class="col-lg-4">
+
+         
+          
+
+          
+
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <?php
+  include 'includes/footer.php';
+  ?>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
 </body>
-<?php
-include 'includes/footer.php';
-?>
+
 </html>
