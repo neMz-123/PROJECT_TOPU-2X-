29f175097a13caed2bda57d87abad4cc417b6267
neMz-123 <143060243+neMz-123@users.noreply.github.com>
2024-05-19 21:10:08 +0800

    modal

--- products.php
+++ products.php
@@ -33,7 +33,7 @@ include 'includes/head.php';
 
 
     <div class="button-row" style="display: flex; justify-content: flex-end;">
-      <button class="btn btn-primary" id="add_employee_button" name="add_employee_button">Add Employee</button>
+      <button class="btn btn-primary" id="add_employee_button" name="add_employee_button"><i class="fa fa-plus" ></i> Add Product</button>
 
     </div>
 
@@ -180,104 +180,108 @@ include 'includes/head.php';
       </div>
     </section>
 
-    <!-- Add Employee Modal -->
-    <div class="modal fade" id="addEmployeeModal" tabindex="-1" role="dialog" aria-labelledby="addEmployeeModalLabel" aria-hidden="true" data-backdrop="static">
-      <div class="modal-dialog modal-xl" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="addEmployeeModalLabel">Add Employee</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form id="addEmployeeForm" class="row" enctype="multipart/form-data" method="POST">
+       <!-- Add Employee Modal -->
+<div class="modal fade" id="addEmployeeModal" tabindex="-1" role="dialog" aria-labelledby="addEmployeeModalLabel" aria-hidden="true" data-backdrop="static">
+  <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-content square-modal">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addEmployeeModalLabel">Add Employee</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      
+      <div class="modal-body">
+      <h4 class="text-primary">Personal Information</h4>
+        <form id="addEmployeeForm" class="row" enctype="multipart/form-data" method="POST">
+        
+          <!-- Personal Information Section -->
+          <div class="col-md-12">
+           
+            <div class="row">
               <!-- Image Section -->
               <div class="col-md-3">
                 <div class="form-group">
                   <label for="image">Image / ID</label>
-                  <div class="image-container">
+                  <!-- <div class="image-container">
                     <img src="" class="img-fluid" alt="" id="previewImage">
-                  </div>
-                  <!-- File Input for Image -->
-                  <input class="form-control" type="file" name="image" id="image" accept=".jpg, .jpeg, .png" value="" />
+                  </div> -->
+                  <input class="form-control" type="file" name="image" id="image" accept=".jpg, .jpeg, .png" />
                 </div>
               </div>
-
-              <!-- Personal Information -->
-
+              <!-- Other Personal Information -->
               <div class="col-md-9">
                 <div class="row">
-                  <div class="col-md-12">
-                    <h4 class="text-primary">Personal Information</h4>
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <label for="age">Age</label>
+                      <input type="number" class="form-control" id="age" name="age" required>
+                    </div>
                   </div>
-                </div>
-
-
-
-
-                <div class="col-md-4">
-                  <div class="form-group">
-                    <label for="age">Age</label>
-                    <input type="number" class="form-control" id="age" name="age" required>
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <label for="start_date">Start Date</label>
+                      <input type="date" class="form-control" id="start_date" name="start_date" required>
+                    </div>
                   </div>
-                </div>
-                <div class="col-md-4">
-                  <div class="form-group">
-                    <label for="start_date">Start Date</label>
-                    <input type="date" class="form-control" id="start_date" name="start_date" required>
-                  </div>
-                </div>
-                <div class="col-md-4">
-                  <div class="form-group">
-                    <label for="salary">Salary</label>
-                    <input type="text" class="form-control" id="salary" name="salary" required>
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <label for="salary">Salary</label>
+                      <input type="text" class="form-control" id="salary" name="salary" required>
+                    </div>
                   </div>
                 </div>
               </div>
-              <br>
-              <!-- Contact Information -->
-              <div class="row">
-                <div class="col-md-12">
-                  <h4 class="text-primary">Contact Information</h4>
+            </div>
+          </div>
+         
+          <!-- Contact Information Section -->
+          <div class="col-md-12">
+            <br>
+            <h4 class="text-primary">Contact Information</h4>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="email">Email Address</label>
+                  <input type="email" class="form-control" id="email" name="email" required>
                 </div>
               </div>
-              <!-- Second Row -->
-              <div class="row">
-                <div class="col-md-4">
-                  <div class="form-group">
-                    <label for="email">Email Address</label>
-                    <input type="email" class="form-control" id="email" name="email" required>
-                  </div>
-                </div>
-                <div class="col-md-4">
-                  <div class="form-group">
-                    <label for="contact">Contact</label>
-                    <input type="tel" class="form-control" id="contact" name="contact" required>
-                  </div>
-                </div>
-
-              </div>
-              <br>
-              <!-- Buttons -->
-              <div class="row justify-content-center">
-                <div class="col-md-3 mb-2 mb-md-0 mr-md-2">
-                  <button type="submit" class="btn btn-primary btn-block">Add</button>
-                </div>
-                <div class="col-md-3 mb-2 mb-md-0 mr-md-2">
-                  <button type="button" class="btn btn-secondary btn-block" id="clearForm">Clear</button>
-                </div>
-                <div class="col-md-3">
-                  <button type="button" class="btn btn-danger btn-block" id="cancelBtn">Cancel</button>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="contact">Contact</label>
+                  <input type="tel" class="form-control" id="contact" name="contact" required>
                 </div>
               </div>
+            </div>
           </div>
 
-
-          </form>
-        </div>
+          
+          <!-- Buttons Section -->
+          
+        </form>
       </div>
+
+      <div class="col-md-12">
+            <div class="row justify-content-center">
+              <div class="col-md-3 mb-2 mb-md-0">
+                <button type="submit" class="btn btn-primary btn-block">Add</button>
+              </div>
+              <div class="col-md-3 mb-2 mb-md-0">
+                <button type="button" class="btn btn-secondary btn-block" id="clearForm">Clear</button>
+              </div>
+              <div class="col-md-3">
+                <button type="button" class="btn btn-danger btn-block" id="cancelBtn">Cancel</button>
+              </div>
+            </div>
+          </div>
+          <br>
     </div>
+  </div>
+</div>
+
+
+
+
     </div>
 
   </main><!-- End #main -->
